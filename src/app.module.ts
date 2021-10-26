83d9e829c9120f2fe23f51365cab33e6b5c9f58c
trod <tobias.bruell94@gmail.com>
2021-10-26 13:58:21 +0200

    Refactoring

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,16 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { EventsService } from './events/events.service';
 import { EventsController } from './events/events.controller';
-import { EventsModule } from './events/events.module';
-import { EventSetModule } from './event-set/event-set.module';
+import { EventSetService } from './event-set/event-set.service';
+import { EventSetController } from './event-set/event-set.controller';
+import { Event, EventSchema } from './schema/adaptionEvent.schema';
+import { Set, SetSchema } from './event-set/schema/set.schema';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/events,'), EventsModule, EventSetModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017/events,'), MongooseModule.forFeature([{name: Event.name, schema: EventSchema},
+  {name: Set.name, schema: SetSchema}])],
+  controllers: [AppController, EventsController, EventSetController],
+  providers: [AppService, EventsService, EventSetService],
 })
 export class AppModule {}
 //'mongodb://mongo-mongodb.default.svc.cluster.local:27017/events,'
--- src/event-set/event-set.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { EventSetService } from './event-set.service';
-import { EventSetController } from './event-set.controller';
-
-@Module({
-  providers: [EventSetService],
-  controllers: [EventSetController]
-})
-export class EventSetModule {}
