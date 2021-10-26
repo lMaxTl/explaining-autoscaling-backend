6aa4d134de20edc928fb48e3aa9e3c4cd750c4a1
trod <tobias.bruell94@gmail.com>
2021-10-26 12:17:33 +0200

    More changes for event relations

--- src/events/events.service.ts
+++ src/events/events.service.ts
@@ -6,9 +6,11 @@ import { Event, EventDocument } from 'src/schema/adaptionEvent.schema';
 
 @Injectable()
 export class EventsService {
-    constructor(@InjectModel(Event.name) private eventModel: Model<EventDocument>) {}
+    constructor(@InjectModel(Event.name) private eventModel: Model<EventDocument>) { }
 
-    receiveNewAdaptionEvent(eventDto : EventDto) {
+    async receiveNewAdaptionEvent(eventDto: EventDto) {
+        let isRelated: boolean;
+        
         const event = new this.eventModel();
         event.createdAt = eventDto.createdAt;
         event.name = eventDto.details.name;
@@ -16,19 +18,16 @@ export class EventsService {
         event.reason = eventDto.details.reason;
         event.message = eventDto.details.message;
         event.scalingType = this.extractScalingType(event);
-        if(this.isRelated(event)) {
-
-        } else {
-
-        }
-        console.log(eventDto);
-        console.log(eventDto.details.name);
-        console.log(eventDto.details.namespace);
-        console.log(eventDto.details.kind);
-        console.log(eventDto.details.message);
-        event.save();
         
-      }
+        await this.isRelated(event).then(function(v) {isRelated = v})
+        if (isRelated) {
+            console.log('isRealted')
+        } else {
+            console.log('isNotRelated')
+        }
+        event.save();
+
+    }
 
     /* convertEventDtoToDbSchema(eventDto: EventDto): Event {
         const event = new this.eventModel();
@@ -39,7 +38,7 @@ export class EventsService {
         event.message = eventDto.details.message;
         return event;
     } */
-    
+
     async create(eventDto: EventDto): Promise<Event> {
         const createEvent = new this.eventModel();
         createEvent.createdAt = eventDto.createdAt;
@@ -49,26 +48,26 @@ export class EventsService {
         createEvent.message = eventDto.details.message;
         return createEvent.save();
     }
-    
+
     async getLatestEvent(): Promise<Event> {
-        return this.eventModel.findOne().sort({'createdAt' : -1}).exec();
+        return this.eventModel.findOne().sort({ 'createdAt': -1 }).exec();
     }
 
     async getAllEvents(): Promise<Event[]> {
         return this.eventModel.find().exec();
     }
-    
+
     async deleteAllEvents() {
         return this.eventModel.remove().exec();
     }
 
-    extractScalingType(event : Event) : string {
+    extractScalingType(event: Event): string {
         let regexScaleUp = /(metrics below target)/g;
         let regexScaleDown = /(above target)/g;
         let scalingType = '';
-        if(event.message.search(regexScaleUp)) {
+        if (event.message.search(regexScaleUp)) {
             scalingType = 'scaleUp'
-        } else if(event.message.search(regexScaleDown)) {
+        } else if (event.message.search(regexScaleDown)) {
             scalingType = 'scaleDown'
         } else {
             scalingType = 'TBD'
@@ -76,25 +75,35 @@ export class EventsService {
         return scalingType;
     }
 
-    isRelated(event : Event): boolean {
-        let latestEvent = this.getLatestEvent();
-
-        if (this.isRelatedByTime() && this.isRelatedByScalingType && this.isRelatedByTime) {
+    async isRelated(event: Event): Promise<boolean> {
+        let latestEvent: Event;
+        await this.getLatestEvent().then(function (v) { latestEvent = v });
+        if (this.isRelatedByTime(event, latestEvent) && this.isRelatedByScalingType(event, latestEvent) && this.isRelatedByDerivative(event, latestEvent)) {
             return true;
         } else {
             return false;
         }
     }
 
-    isRelatedByTime(): boolean {
-        return null;
+    isRelatedByTime(event: Event, latestEvent: Event): boolean {
+        const currentDate = new Date(event.createdAt);
+        const latestDate = new Date(latestEvent.createdAt)
+        let difference = Math.abs(currentDate.getTime() - latestDate.getTime());
+        console.log(difference);
+        return true;
     }
 
-    isRelatedByDerivative(): boolean {
-        return null;
+    isRelatedByDerivative(event: Event, latestEvent: Event): boolean {
+        console.log('derivative')
+        return true;
     }
 
-    isRelatedByScalingType(): boolean {
-        return null;
+    isRelatedByScalingType(event: Event, latestEvent: Event): boolean {
+        if (event.scalingType === latestEvent.scalingType) {
+            console.log('scalingType matches')
+            return true;
+        } else {
+            return false;
+        }
     }
 }
